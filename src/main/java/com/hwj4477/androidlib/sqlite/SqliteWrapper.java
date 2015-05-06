79fdd238a363a89c5ff8ef8e5a91f595755dd349
wjhong <wjhong@st-company.net>
2015-05-06 16:36:23 +0900

    edit SqliteWrapper

--- src/main/java/com/hwj4477/androidlib/sqlite/SqliteWrapper.java
+++ src/main/java/com/hwj4477/androidlib/sqlite/SqliteWrapper.java
@@ -1,5 +1,11 @@
 package com.hwj4477.androidlib.sqlite;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,12 +13,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 
 /**
  * 
@@ -78,8 +78,55 @@ public class SqliteWrapper {
 			return;
 		}
 	}
-	
-	@SuppressLint("NewApi")
+
+    @SuppressLint("NewApi")
+    public ArrayList<HashMap<String, Object>> selectQuery(boolean distinct, String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy, String limit)
+    {
+        Cursor cursor = db.query(distinct, table, columns, selection, selectionArgs, groupBy, having, orderBy, limit);
+
+        if (cursor.moveToFirst())
+        {
+            ArrayList<HashMap<String, Object>> arrayResult = new ArrayList<HashMap<String, Object>>();
+
+            do
+            {
+                HashMap<String, Object> data = new HashMap<String, Object>();
+
+                for(int i=0; i<cursor.getColumnCount(); i++)
+                {
+                    switch (cursor.getType(i))
+                    {
+                        case Cursor.FIELD_TYPE_INTEGER:
+                            data.put(cursor.getColumnName(i), cursor.getInt(i));
+                            break;
+                        case Cursor.FIELD_TYPE_FLOAT:
+                            data.put(cursor.getColumnName(i), cursor.getFloat(i));
+                            break;
+                        case Cursor.FIELD_TYPE_STRING:
+                            data.put(cursor.getColumnName(i), cursor.getString(i));
+                            break;
+                        case Cursor.FIELD_TYPE_BLOB:
+                            data.put(cursor.getColumnName(i), cursor.getBlob(i));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                arrayResult.add(data);
+
+            }while (cursor.moveToNext());
+
+            cursor.close();
+
+            return arrayResult;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
 	public ArrayList<HashMap<String, Object>> selectQuery(String query)
 	{
 		Cursor cursor = db.rawQuery(query, null);
@@ -127,9 +174,9 @@ public class SqliteWrapper {
 		}
 	}
 	
-	public boolean insert(String table, ContentValues values)
+	public boolean insert(String table, String nullColumnHack, ContentValues values)
 	{
-		return db.insert(table, null, values) != -1;
+		return db.insert(table, nullColumnHack, values) != -1;
 	}
 	
 	public boolean update(String table, ContentValues values, String whereClause, String[] whereArgs)
